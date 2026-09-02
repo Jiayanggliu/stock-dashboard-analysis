eef501cf0298dcf3ef35ad09770165a89446907a
Jiayanggliu <jiayanggliu@gmail.com>
2026-09-02 00:42:34 -0700

    Add initial Streamlit dashboard for stock analysis

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Retail Investor Stock Dashboard")
+
+st.write(
+  "An interactive dashboard for comparing stock performance,"
+  "momentum, volatility, and trent."
+)
